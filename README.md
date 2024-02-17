3cb5b50a890d9d73287a5bedfb833e6e25e8aa12
Samuel Nunes Sergio <samuel.sergio@me.com.br>
2024-02-17 04:04:45 -0300

    Commit Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+### Instructions
+```bash
+# Create Docker 
+docker-compose up --build -d
+
+# Access route
+curl http://localhost:3000
+
+# Run tests
+docker-compose exec app npm test
+```
---
+++ app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('./app')
+
+describe('Test the root path', () => {
+  test('It should response the GET method', done => {
+    request(app)
+      .get('/')
+      .then(response => {
+        expect(response.statusCode).toBe(202)
+        expect(response.body.message).toBe('Hello World!')
+        done()
+      })
+  })
+})
